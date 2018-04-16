37d86b5737b47ec1d10533a8d9c5d54589c8b577
rurigrass <ruripg@gmail.com>
2018-04-16 18:08:20 +0100

    finished

--- lab_classes_part_b.rb
+++ lab_classes_part_b.rb
@@ -1,24 +1,46 @@
 
 class Team
 
-# attr_reader(:name, :players, :coach)
+attr_accessor(:name, :players, :coach)
 
 def initialize(name, players, coach)
   @name = name
   @players = players
   @coach = coach
+  @points = 0
 end
 
-def name()
-  return @name
+# def name()
+#   return @name
+# end
+#
+# def players()
+#   return @players
+# end
+#
+# def coach()
+#   return @coach
+# end
+
+def set_coach(new_name)
+  return  @coach = new_name
 end
 
-def players()
-  return @players
+def add_players(new_player)
+  return  @players.push(new_player)
 end
 
-def coach()
-  return @coach
+def check_players(find_player)
+  return @players.include?(find_player)
 end
+
+def update_team_points(win_or_lose)
+   result = {
+     "win" => 3,
+     "lose" => 0
+   }
+   @points += result[win_or_lose]
+   
+  end
 
 end
--- specs/lab_classes_part_b_spec.rb
+++ specs/lab_classes_part_b_spec.rb
@@ -19,5 +19,37 @@ class TestTeam < MiniTest::Test
     assert_equal("Zsolt", club.coach())
   end
 
+  def test_set_coach
+    club = Team.new("Codeclan FC",["Luis", "Ruairidh"], "Zsolt")
+    club.set_coach ("Sandy")
+    assert_equal("Sandy", club.coach())
+  end
+
+  def test_add_players
+    club = Team.new("Codeclan FC",["Luis", "Ruairidh"], "Zsolt")
+    club.add_players ("Duncan")
+    assert_equal(["Luis", "Ruairidh", "Duncan"], club.players())
+  end
+
+  def test_check_players
+    club = Team.new("Codeclan FC",["Luis", "Ruairidh"], "Zsolt")
+    club.check_players ("Luis")
+    assert_equal(true , club.check_players("Luis"))
+  end
+
+  def test_team_won
+    club = Team.new("Codeclan FC", ["Luis", "Colin"], "Zsolt")
+    win = club.update_team_points("win")
+    assert_equal(3, win)
+  end
+
+  def test_team_lose
+    club = Team.new("Codeclan FC", ["Luis", "Colin"], "Zsolt")
+    lose = club.update_team_points("lose")
+    assert_equal(0, lose)
+  end
+
+
+
 
 end
